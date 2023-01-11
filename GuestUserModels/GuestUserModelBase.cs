da76f6b778ba684a4243ff761198be14991a7462
ranjith gowda <ranjith.1409@gmail.com>
2023-01-11 11:27:44 +0300

    Guest Visitor Model Updated

---
+++ GuestUserModels/GuestUserModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestUserModels
+{
+    class GuestModelBase
+    {
+        public string OfficalID { get; set; }
+        public string ExpiryDate { get; set; }
+        public string VisitorCompanyName { get; set; }
+        public string VisitorFirstName { get; set; }
+        public string VisitorSecondName { get; set; }
+        public string VisitorIDBadgeNo { get; set; }
+        public string PurposeOfVisit { get; set; }
+        public string DateOfVisit { get; set; }
+        public string VisitorArraival { get; set; }
+        public string VisitorDeparture { get; set; }
+        public string EmployeeToVisitName { get; set; }
+        public string EmployeeToVisitId { get; set; }
+
+    }
+}
