57949812b5c299e18a781d2de193b9188072b2eb
Ranjith <rnagaraja@moi.gov.qa>
2023-04-09 14:45:02 +0300

    Calibrum micro added view model deskUI project

---
+++ GuestRegistrationDeskUI/Bootstrapper.cs
@@ -0,0 +1,68 @@
+﻿using Caliburn.Micro;
+using GuestRegistrationDeskUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GuestRegistrationDeskUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+
+        public Bootstrapper()
+        {
+            Initialize();
+
+            //ConventionManager.AddElementConvention<PasswordBox>(
+            //PasswordBoxHelper.BoundPasswordProperty,
+            //"Password",
+            //"PasswordChanged");
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+                //.Singleton<ILoggedInUserModel, LoggedInUserModel>()
+                //.Singleton<IAPIHelper, APIHelper>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
+            //base.Configure();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+            //base.OnStartup(sender, e);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+            //base.BuildUp(instance);
+        }
+    }
+}
