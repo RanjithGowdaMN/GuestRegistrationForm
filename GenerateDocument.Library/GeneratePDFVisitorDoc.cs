7d11e4efb76c1c13e5eceeba7dcf85af248c4e3a
Ranjith <rnagaraja@moi.gov.qa>
2023-08-02 10:18:04 +0300

    Generate Document Lib update

---
+++ GenerateDocument.Library/GeneratePDFVisitorDoc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateDocument.Library
+{
+    public class GeneratePDFVisitorDoc
+    {
+        public void GenerateVisitorDocument(GuestDataModel guestDataModel, string inputFilePath,
+                                            string outputFilePath, string imagePath)
+        {
+            inputFilePath = @"D:\VisitorData\BaseDocument\Visitor.pdf";
+            string fullName = guestDataModel.Name;
+            string[] nameParts = fullName.Split(' ');
+
+            string firstName = nameParts[1];
+            string scndName = nameParts[2];
+            string lastName = string.Join(" ", nameParts.Skip(3));
+            // adding text
+            List<Tuple<float, float, string>> textsToAdd1 = new List<Tuple<float, float, string>>
+            {
+
+            new Tuple<float, float, string>(390, 580, firstName),
+               new Tuple<float, float, string>(230, 580, scndName),
+                new Tuple<float, float, string>(70, 580, lastName),
+              /*  new Tuple<float, float, string>(340, 550, "ad"),
+                new Tuple<float, float, string>(240, 550, "city"),
+                new Tuple<float, float, string>(140, 550, "stste"),
+                new Tuple<float, float, string>(70, 536, "zip"),
+                new Tuple<float, float, string>(315, 513, "email"),
+                new Tuple<float, float, string>(210, 513, "cell"),
+                new Tuple<float, float, string>(80, 513, "tel"),
+                new Tuple<float, float, string>(310, 483, "ss"),
+                new Tuple<float, float, string>(110, 453, "cn"),*/
+                new Tuple<float, float, string>(310, 415, guestDataModel.IDno),
+              /*  new Tuple<float, float, string>(100, 415, "pn"),
+                new Tuple<float, float, string>(310, 383, "pd"),
+                new Tuple<float, float, string>(100, 383, "pv"),
+                new Tuple<float, float, string>(210, 350, "purpos"),
+                new Tuple<float, float, string>(310, 315, "dura"),
+                new Tuple<float, float, string>(110, 315, "ec"),
+                new Tuple<float, float, string>(110, 260, "ex"),*/
+
+            };
+            List<Tuple<float, float, string>> textsToAdd = new List<Tuple<float, float, string>>
+            {
+                new Tuple<float, float, string>(110, 710, guestDataModel.Name),
+               // new Tuple<float, float, string>(310, 415, guestDataModel.IDno),
+                //new Tuple<float, float, string>(350, 680,guestDataModel.Expiry),
+                
+                // To be add other data
+
+            };
+
+            outputFilePath = $"D:\\VisitorData\\GeneratedDocument\\V_{guestDataModel.Name}.pdf";
+
+            // Modify the PDF with the image and texts
+            GeneratePDFdocument.ModifyPdf(inputFilePath, textsToAdd1, textsToAdd, outputFilePath, imagePath);
+        }
+    }
+}
--- GenerateDocument.Library/GeneratePDFdocument.cs
+++ GenerateDocument.Library/GeneratePDFdocument.cs
@@ -14,56 +14,23 @@ namespace GenerateDocument.Library
         public void GeneratePdfDoc(GuestDataModel guestDataModel, string inputFilePath,
                                     string outputFilePath, string imagePath, string docType)
         {
-            inputFilePath = @"D:\VisitorData\BaseDocument\Contractor.pdf";
-            string fullName = guestDataModel.Name;
-            string[] nameParts = fullName.Split(' ');
-
-            string firstName = nameParts[1];
-            string scndName = nameParts[2];
-            string lastName = string.Join(" ", nameParts.Skip(3));
-            // adding text
-            List<Tuple<float, float, string>> textsToAdd1 = new List<Tuple<float, float, string>>
+            if (docType == "visitor")
             {
-           
-            new Tuple<float, float, string>(390, 580, firstName),
-               new Tuple<float, float, string>(230, 580, scndName),
-                new Tuple<float, float, string>(70, 580, lastName),
-              /*  new Tuple<float, float, string>(340, 550, "ad"),
-                new Tuple<float, float, string>(240, 550, "city"),
-                new Tuple<float, float, string>(140, 550, "stste"),
-                new Tuple<float, float, string>(70, 536, "zip"),
-                new Tuple<float, float, string>(315, 513, "email"),
-                new Tuple<float, float, string>(210, 513, "cell"),
-                new Tuple<float, float, string>(80, 513, "tel"),
-                new Tuple<float, float, string>(310, 483, "ss"),
-                new Tuple<float, float, string>(110, 453, "cn"),*/
-                new Tuple<float, float, string>(310, 415, guestDataModel.IDno),
-              /*  new Tuple<float, float, string>(100, 415, "pn"),
-                new Tuple<float, float, string>(310, 383, "pd"),
-                new Tuple<float, float, string>(100, 383, "pv"),
-                new Tuple<float, float, string>(210, 350, "purpos"),
-                new Tuple<float, float, string>(310, 315, "dura"),
-                new Tuple<float, float, string>(110, 315, "ec"),
-                new Tuple<float, float, string>(110, 260, "ex"),*/
-
-            };
-            List<Tuple<float, float, string>> textsToAdd = new List<Tuple<float, float, string>>
+                GeneratePDFVisitorDoc generatePDFVisitorDoc = new GeneratePDFVisitorDoc();
+                generatePDFVisitorDoc.GenerateVisitorDocument(guestDataModel, inputFilePath, outputFilePath, imagePath);
+            }
+            else if (docType == "contract")
             {
-                new Tuple<float, float, string>(110, 710, guestDataModel.Name),
-               // new Tuple<float, float, string>(310, 415, guestDataModel.IDno),
-                //new Tuple<float, float, string>(350, 680,guestDataModel.Expiry),
-                
-                // To be add other data
+                GeneratePDfContractDoc generatePDfContractDoc = new GeneratePDfContractDoc();
+                generatePDfContractDoc.GenerateContractDocument(guestDataModel, inputFilePath, outputFilePath, imagePath);
+            }
+            else
+            {
 
-            };
-
-            outputFilePath = $"D:\\VisitorData\\GeneratedDocument\\Visotor_{guestDataModel.IDno}.pdf";
-
-            // Modify the PDF with the image and texts
-            ModifyPdf(inputFilePath, textsToAdd1, textsToAdd, outputFilePath, imagePath);
+            }
         }
 
-        private void ModifyPdf(string inputFilePath, List<Tuple<float, float, string>> textsToAdd1, List<Tuple<float, float, string>> textsToAdd,
+        public static void ModifyPdf(string inputFilePath, List<Tuple<float, float, string>> textsToAdd1, List<Tuple<float, float, string>> textsToAdd,
                                 string outputFilePath, string imagePath)
         {
             // Check if the PDF and image files exist
@@ -106,7 +73,7 @@ namespace GenerateDocument.Library
             }
         }
 
-        private void AddTextToPdf(PdfStamper pdfStamper, List<Tuple<float, float, string>> textsToAdd)
+        public static void AddTextToPdf(PdfStamper pdfStamper, List<Tuple<float, float, string>> textsToAdd)
         {
             try
             {
@@ -134,7 +101,7 @@ namespace GenerateDocument.Library
             }
         }
 
-       private void AddTextToTargetPage(PdfStamper pdfStamper, List<Tuple<float, float, string>> textsToAdd1)
+        public static void AddTextToTargetPage(PdfStamper pdfStamper, List<Tuple<float, float, string>> textsToAdd1)
         {
             try
             {
@@ -162,7 +129,7 @@ namespace GenerateDocument.Library
             }
         }
 
-        private void AddImageToPdf(PdfStamper pdfStamper, string imagePath)
+        public static void AddImageToPdf(PdfStamper pdfStamper, string imagePath)
         {
             try
             {
