caf3f6da453950a744c3bb16a9ea37f8aea098bb
Admin <Admin@JIJO-DEV>
2023-08-09 09:39:27 +0300

    Reverted Text Processing

--- GuestRegistrationDesktopUI.Library/TextProcessing/ProcessTextData.cs
+++ GuestRegistrationDesktopUI.Library/TextProcessing/ProcessTextData.cs
@@ -1,5 +1,4 @@
 ﻿using GuestRegistrationDesktopUI.Library.Models;
-using NLog;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,17 +12,16 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
     public class ProcessTextData
     {
         private const string TextProcessingErrorMessage = "Error in text processing! Please ReScan";
-        private static Logger logger = LogManager.GetCurrentClassLogger();
         public VisitorDataModel ProcessTextFromBlob(string InputText)
         {
             VisitorDataModel visitorData = new VisitorDataModel();
-            
+
             if (InputText.Length < 25)
             {
                 return FillErrorMessage();
             }
 
-            bool isQatarId = InputText.Contains("State Of Qatar") || InputText.Contains("Residency Permit") || InputText.Contains("Permit"); 
+            bool isQatarId = InputText.Contains("State Of Qatar") || InputText.Contains("Residency Permit") || InputText.Contains("Permit");
 
             if (isQatarId)
             {
@@ -35,24 +33,6 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
             }
 
             //return visitorData;
-        }
-            try
-            {
-                bool isQatarId = InputText.Contains("State Of Qatar") || InputText.Contains("Residency Permit") || InputText.Contains("Permit");
-                if (isQatarId)
-                {
-                    visitorData = ProcessQatarIdImage(InputText);
-                }
-                else
-                {
-                    visitorData = ProcessPassPortImage(InputText);
-                }
-            }
-            catch (Exception e)
-            {
-                logger.Error($"Error: {e.Message}");
-            }
-            return visitorData;
         }
         private List<string> RemoveEmptyItems(List<string> items)
         {
@@ -90,7 +70,7 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
             //    }
             //}
 
-            visitorDataModel.Name = FirstName + " "+secondName;
+            visitorDataModel.Name = FirstName + " " + secondName;
             visitorDataModel.IDno = Idnumber;
             visitorDataModel.Nationality = countryCode;
             visitorDataModel.DateOfBirth = DateOfBirth;
@@ -98,119 +78,64 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
             visitorDataModel.IsPassport = true;
             return visitorDataModel;
         }
-            try
-            {
-                VisitorDataModel visitorDataModel = new VisitorDataModel();
-                //VisitorDataModel visitorDataModel = new VisitorDataModel();
-                string mrz = inputText.Substring(inputText.IndexOf("<")).Replace("\n", "");
-                List<string> mrzCode2Part = mrz.Split(new[] { "<<" }, StringSplitOptions.None).ToList();
-                mrzCode2Part = RemoveEmptyItems(mrzCode2Part);
-                string countryCode = mrzCode2Part[0].Substring(1, 3);
-                string secondName = mrzCode2Part[0].Substring(4).Replace("<", " ");
-                List<string> SecondPartParsed = mrzCode2Part[1].Split('<').ToList();
-                string FirstName = ExtractFirstName(SecondPartParsed);
-                (string Idnumber, int startIndexOfDOB) = ExtractIDnumber(SecondPartParsed);
-                string remaingData = SecondPartParsed[startIndexOfDOB];
-                remaingData = remaingData.Substring(3).Replace(" ", "");
-                (string DateOfBirth, int dobIndex) = ExtractNumberGroup(remaingData, 0);
-                (string ExpiryDate, int expIndex) = ExtractNumberGroup(remaingData, dobIndex + 1);
-                visitorDataModel.Name = FirstName + " " + secondName;
-                visitorDataModel.IDno = Idnumber;
-                visitorDataModel.Nationality = countryCode;
-                visitorDataModel.DateOfBirth = DateOfBirth;
-                visitorDataModel.Expiry = ExpiryDate;
-                return visitorDataModel;
-            }
-            catch (Exception e)
-            {
-                logger.Error($"Error: {e.Source} /n {e.Message} /n {e.InnerException} /n {e.StackTrace}");
-                throw;
-            }
-
-        }
         private (string result, int curIndex) ExtractNumberGroup(string currentItem, int startIdx)
         {
-            try
+            string result = string.Empty;
+            int count = 0;
+            for (int i = startIdx; i < currentItem.Length; i++)
             {
-                string result = string.Empty;
-                int count = 0;
-                for (int i = startIdx; i < currentItem.Length; i++)
+                if (Char.IsNumber(currentItem[i]))
                 {
-                    if (Char.IsNumber(currentItem[i]))
-                    {
-                        result = currentItem.Substring(i, 6);
-                        count = i + 6;
-                        break;
-                    }
+                    result = currentItem.Substring(i, 6);
+                    count = i + 6;
+                    break;
                 }
-                return (DateInterpolation(result), count);
             }
-            catch (Exception e)
-            {
-                logger.Error($"Error: ExtractNumberGroup {e.Message}--- {currentItem}");
-                throw; 
-            }
-
+            return (DateInterpolation(result), count);
         }
         private (string, int) ExtractIDnumber(List<string> items)
         {
-            try
+            string IdNumber = string.Empty;
+            int count = 0;
+            foreach (var item in items)
             {
-                string IdNumber = string.Empty;
-                int count = 0;
-                foreach (var item in items)
+                count++;
+                foreach (var chars in item)
                 {
-                    count++;
-                    foreach (var chars in item)
+                    if (Char.IsNumber(chars))
                     {
-                        if (Char.IsNumber(chars))
-                        {
-                            IdNumber = item;
-                            break;
-                        }
-                    }
-                    if (IdNumber != "")
-                    {
+                        IdNumber = item;
                         break;
                     }
                 }
-                return (IdNumber, count);
+                if (IdNumber != "")
+                {
+                    break;
+                }
             }
-            catch (Exception e)
-            {
-                logger.Error($"Error in ExtractIDnumber {items[0]} /n {items[1]} /n {items[2]}");
-                throw;
-            }
+            return (IdNumber, count);
         }
         private string ExtractFirstName(List<string> items)
         {
-            try
+            StringBuilder sb = new StringBuilder();
+            bool anyNumPresent = false;
+            foreach (var item in items)
             {
-                StringBuilder sb = new StringBuilder();
-                bool anyNumPresent = false;
-                foreach (var item in items)
+                foreach (var chars in item)
                 {
-                    foreach (var chars in item)
+                    if (Char.IsNumber(chars))
                     {
-                        if (Char.IsNumber(chars))
-                        {
-                            anyNumPresent = true;
-                            break;
-                        }
-                    }
-                    if (!anyNumPresent)
-                    {
-                        sb.Append(item);
-                        sb.Append(" ");
+                        anyNumPresent = true;
+                        break;
                     }
                 }
-                return sb.ToString().Trim();
+                if (!anyNumPresent)
+                {
+                    sb.Append(item);
+                    sb.Append(" ");
+                }
             }
-            catch (Exception e)
-            {
-                logger.Error($"Error in  ExtractFirstName {items[0]} \n {items[1]} \n {items[2]}");
-                throw;
-            }
+            return sb.ToString().Trim();
         }
         private VisitorDataModel ProcessQatarIdImage(string inputText)
         {
@@ -262,51 +187,20 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
 
             return visitorDataModel;
         }
-            try
-            {
-                List<string> data = new List<string>();
-                VisitorDataModel visitorDataModel = new VisitorDataModel();
-                using (StringReader reader = new StringReader(inputText))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        string text = line.TrimStart().TrimEnd();
-                        if (text != null && text != string.Empty && text != "")
-                        {
-                            data.Add(text);
-                        }
-                    }
-                }
-                return visitorDataModel = ExtractIdDataByImageFields(data);
-            }
-            catch (Exception e)
-            {
-                logger.Error($" Error in ProcessQatarIdImage!!");
-                throw;
-            }
-        }
 
         private string ExtractDate(string InputText)
         {
-            try
+            string ddmmyyyy = string.Empty;
+            bool foundFWslash = InputText.Contains("/");
+            if (foundFWslash)
             {
-                string ddmmyyyy = string.Empty;
-                bool foundFWslash = InputText.Contains("/");
-                if (foundFWslash)
+                int firstIdx = InputText.IndexOf("/");
+                int secondIdx = InputText.IndexOf("/", firstIdx + 1);
+                if (secondIdx - firstIdx == 3)
                 {
-                    int firstIdx = InputText.IndexOf("/");
-                    int secondIdx = InputText.IndexOf("/", firstIdx + 1);
-                    if (secondIdx - firstIdx == 3)
-                    {
-                        ddmmyyyy = InputText.Substring(firstIdx - 2, secondIdx - 2);
-                        return ddmmyyyy;
-                    }
+                    ddmmyyyy = InputText.Substring(firstIdx - 2, secondIdx - 2);
+                    return ddmmyyyy;
                 }
-            }
-            catch (Exception)
-            {
-                logger.Error($" Error in ExtractDate {InputText}");
             }
             return "Error/ Please rescan or enter manually";
         }
@@ -337,6 +231,7 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
                     sb.Append(c);
                 }
             }
+
             return sb.ToString();
         }
 
@@ -350,76 +245,66 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
             {
                 result += match.Value;
             }
+
             return result;
         }
         private string DateInterpolation(string item)
         {
-            try
+            StringBuilder number = new StringBuilder();
+            foreach (var chars in item)
             {
-                StringBuilder number = new StringBuilder();
-                foreach (var chars in item)
+                if (Char.IsNumber(chars))
                 {
-                    if (Char.IsNumber(chars))
-                    {
-                        number.Append(chars);
-                    }
+                    number.Append(chars);
                 }
-                string year = number.ToString().Substring(0, 2);
-                string month = number.ToString().Substring(2, 2);
-                string day = number.ToString().Substring(4, 2);
-                int yearinInt = Convert.ToInt32(year);
-                if (yearinInt <= 50 && yearinInt >= 00)
-                {
-                    year = "20" + year;
-                }
-                else
-                {
-                    year = "19" + year;
-                }
-                number.Clear();
-                number.Append(day);
-                number.Append("/");
-                number.Append(month);
-                number.Append("/");
-                number.Append(year);
-                return number.ToString();
             }
-            catch (Exception e)
+            string year = number.ToString().Substring(0, 2);
+            string month = number.ToString().Substring(2, 2);
+            string day = number.ToString().Substring(4, 2);
+            int yearinInt = Convert.ToInt32(year);
+            if (yearinInt <= 50 && yearinInt >= 00)
             {
-                logger.Error($"Error in DateInterpolation {item}");
-                throw;
+                year = "20" + year;
             }
-            
+            else
+            {
+                year = "19" + year;
+            }
+            number.Clear();
+            number.Append(day);
+            number.Append("/");
+            number.Append(month);
+            number.Append("/");
+            number.Append(year);
+            return number.ToString();
         }
 
-        private VisitorDataModel ExtractIdDataByImageFields(List<string> data)
+        private VisitorDataModel ExtractPassportDataByImageFields(List<string> data)
         {
-            try
+            VisitorDataModel visitorDataModel = new VisitorDataModel();
+            foreach (string line in data)
             {
-                VisitorDataModel visitorDataModel = new VisitorDataModel();
-                foreach (string line in data)
+                if (line.Contains("ID"))
                 {
-                    if (line.Contains("ID"))
+                    //visitorDataModel.IDno = line.Substring(line.IndexOf(":") + 1, 12);
+                    try
                     {
-                        //visitorDataModel.IDno = line.Substring(line.IndexOf(":") + 1, 12);
-                        try
-                        {
-                            visitorDataModel.IDno = SelectNumberOnly(line);
-                        }
-                        catch (Exception)
-                        {
-
-                            visitorDataModel.IDno = TextProcessingErrorMessage;
-                        }
+                        visitorDataModel.IDno = SelectNumberOnly(line);
                     }
-                    else if (line.Contains("D.O.B") || line.Contains("D.0.B") || line.Contains("D.O") || line.Contains(".O."))
+                    catch (Exception)
                     {
-                        try
-                        {
-                            visitorDataModel.DateOfBirth = ExtractDate(line);
-                        }
-                        catch (Exception)
-                        {
+
+                        visitorDataModel.IDno = TextProcessingErrorMessage;
+                    }
+                }
+                else if (line.Contains("D.O.B") || line.Contains("D.0.B") || line.Contains("D.O") || line.Contains(".O."))
+                {
+                    try
+                    {
+                        visitorDataModel.DateOfBirth = ExtractDate(line);
+                    }
+                    catch (Exception)
+                    {
 
                         visitorDataModel.DateOfBirth = TextProcessingErrorMessage;
                     }
@@ -460,7 +345,7 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
             }
             return visitorDataModel;
         }
-    
+
         private VisitorDataModel FillErrorMessage()
         {
             VisitorDataModel visitorDataModel = new VisitorDataModel();
@@ -471,53 +356,7 @@ namespace GuestRegistrationDesktopUI.Library.TextProcessing
             visitorDataModel.Nationality = "Error Please Re-Scan";
             return visitorDataModel;
         }
-                            visitorDataModel.DateOfBirth = TextProcessingErrorMessage;
-                        }
-                    }
-                    else if (line.Contains("Expiry"))
-                    {
-                        try
-                        {
-                            visitorDataModel.Expiry = ExtractDate(line);
-                        }
-                        catch (Exception)
-                        {
-                            visitorDataModel.Expiry = TextProcessingErrorMessage;
-                        }
-                    }
-                    else if (line.Contains("Nationality"))
-                    {
-                        try
-                        {
-                            visitorDataModel.Nationality = RemoveLowerCase(line.Substring(line.IndexOf(":") + 1));
-                        }
-                        catch (Exception)
-                        {
-                            visitorDataModel.Nationality = TextProcessingErrorMessage;
-                        }
-                    }
-                    else if (line.Contains("Name") || line.Contains("Sam") || line.Contains("|") || line.Contains("ame"))
-                    {
-                        try
-                        {
-                            visitorDataModel.Name = RemoveLowerCase(line.Substring(line.IndexOf(":") + 1));
-                        }
-                        catch (Exception)
-                        {
-                            visitorDataModel.Name = TextProcessingErrorMessage;
-                        }
-                    }
-                }
-                return visitorDataModel;
-            }
-            catch (Exception)
-            {
-                logger.Error($"Error in ExtractIdDataByImageFields {data[0]} /n  {data[1]} /n {data[2]} ");
-                throw;
-            }
-            
-        }
     }
-    
+
 
 }
