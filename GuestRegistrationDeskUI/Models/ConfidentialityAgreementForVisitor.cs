2171e5e09f69b1523f93b44878e292b581c2d4a5
Admin <Admin@JIJO-DEV>
2023-08-23 08:06:05 +0300

    Visitor Data models added

---
+++ GuestRegistrationDeskUI/Models/ConfidentialityAgreementForVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestRegistrationDeskUI.Models
+{
+    class ConfidentialityAgreementForVisitor
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Date { get; set; }
+    }
+}
