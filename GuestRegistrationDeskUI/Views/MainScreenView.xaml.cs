d79da4502bc8eb557ede3a12c1edb0907017c873
Ranjith <rnagaraja@moi.gov.qa>
2023-09-21 07:02:47 +0300

    using Directive updated

--- GuestRegistrationDeskUI/Views/MainScreenView.xaml.cs
+++ GuestRegistrationDeskUI/Views/MainScreenView.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Caliburn.Micro;
 using Ghostscript.NET.Rasterizer;
-using GuestRegistrationDesktopUI.Library.CentralHub;
 using GuestRegistrationDeskUI.Models;
 using GuestRegistrationDeskUI.ViewModels;
 using Newtonsoft.Json;
@@ -10,22 +9,11 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Drawing.Printing;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 //using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
-using Windows.Storage;
 
 namespace GuestRegistrationDeskUI.Views
 {
