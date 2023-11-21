b02e234642417f11e8d731a53877c1134a09aaa3
Ranjith <rnagaraja@moi.gov.qa>
2023-11-21 06:35:30 +0300

    DB testing

---
+++ GuestDataManager.Library/DataAccess/InsertData.cs
@@ -0,0 +1,23 @@
+﻿using GuestDataManager.Library.Internal.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestDataManager.Library.DataAccess
+{
+    public class InsertData
+    {
+        public void GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            //var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "GuestData");
+
+            //return output;
+        }
+    }
+}
