d03a93eba39872cd49398a9cac88a06a5e521029
Ranjith <rnagaraja@moi.gov.qa>
2023-11-21 09:27:05 +0300

    git Clash resolved

--- GuestRegistrationWinForm/FormVisitor.cs
+++ GuestRegistrationWinForm/FormVisitor.cs
@@ -16,7 +16,7 @@ namespace gui
 {
     public partial class FormVisitor : Form
     {
-        public  ICentralHub _centralHub;
+        public ICentralHub _centralHub;
         private ScannedFileModel _scannedFileInfo;
         private ScannedData _scannedData;
         private CameraStatus _cameraStatus;
@@ -38,30 +38,34 @@ namespace gui
             txtVisitorComp.TextChanged += TextChanged;
             txtVisitorSecutityController.TextChanged += TextChanged;
 
-
+            txtVisitorTitle.Text = _visitorDataSheet.Title;
+            txtVisitorComp.Text = _visitorDataSheet.Company;
+            txtVisitorSecutityController.Text = _visitorDataSheet.SecurityController;
+            dtVisitorVisitDate.Text = _visitorDataSheet.VisitDateTime;
+            _visitorDataSheet.VisitDuration = dtVisitorDuration.Text;
         }
-        private void TextChanged(Object sender,EventArgs e)
+        private void TextChanged(Object sender, EventArgs e)
         {
             TextBox tb = (TextBox)sender;
-            if(tb.Name==txtVisitorTitle.Name)
+            if (tb.Name == txtVisitorTitle.Name)
             {
                 _visitorDataSheet.Title = txtVisitorTitle.Text;
             }
-            if(tb.Name==txtVisitorComp.Name)
+            if (tb.Name == txtVisitorComp.Name)
             {
                 _visitorDataSheet.Company = txtVisitorComp.Text;
 
             }
-            if(tb.Name==txtVisitorSecutityController.Name)
+            if (tb.Name == txtVisitorSecutityController.Name)
             {
                 _visitorDataSheet.SecurityController = txtVisitorSecutityController.Text;
             }
-            if(tb.Name== dtVisitorVisitDate.Name)
+            if (tb.Name == dtVisitorVisitDate.Name)
             {
                 _visitorDataSheet.VisitDateTime = dtVisitorVisitDate.Text.ToString();
 
             }
-            if(tb.Name==dtVisitorDuration.Name)
+            if (tb.Name == dtVisitorDuration.Name)
             {
                 _visitorDataSheet.VisitDuration = dtVisitorDuration.Text.ToString();
             }
