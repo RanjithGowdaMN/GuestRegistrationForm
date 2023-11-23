a32474c37922813879c678c09a4ea59eca343166
Ranjith <rnagaraja@moi.gov.qa>
2023-11-23 10:22:07 +0300

    Table Added, conflict resolved,

---
+++ GuestData/dbo/Tables/VisitorInformation.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[VisitorInformation]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] nvarchar(50) Not null,
+	[IdNumber] nvarchar(50) not null,
+	[DoB] nvarchar(15) not null,
+	[IdExpiry] nvarchar(15) not null,
+	[Nationality] nvarchar(15) not null,
+	[IdType] int not null,
+
+)
