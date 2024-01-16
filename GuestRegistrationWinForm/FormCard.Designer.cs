a44232c82a9054cc5473ee55680422bc327e7822
Admin <Admin@JIJO-DEV>
2024-01-16 10:26:20 +0300

    card initial design

--- GuestRegistrationWinForm/FormCard.Designer.cs
+++ GuestRegistrationWinForm/FormCard.Designer.cs
@@ -30,25 +30,131 @@ namespace gui
         private void InitializeComponent()
         {
             this.panelCard = new System.Windows.Forms.Panel();
+            this.txtCardId = new System.Windows.Forms.TextBox();
+            this.panelCardDemo = new System.Windows.Forms.Panel();
+            this.pbCardDemo = new System.Windows.Forms.PictureBox();
+            this.lblCardType = new System.Windows.Forms.Label();
+            this.lblCardName = new System.Windows.Forms.Label();
+            this.lblCardIdno = new System.Windows.Forms.Label();
+            this.btnCardSearch = new System.Windows.Forms.Button();
+            this.btnCardPrint = new System.Windows.Forms.Button();
+            this.panelCard.SuspendLayout();
+            this.panelCardDemo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCardDemo)).BeginInit();
             this.SuspendLayout();
             // 
             // panelCard
             // 
+            this.panelCard.BackColor = System.Drawing.Color.White;
+            this.panelCard.Controls.Add(this.btnCardPrint);
+            this.panelCard.Controls.Add(this.btnCardSearch);
+            this.panelCard.Controls.Add(this.lblCardIdno);
+            this.panelCard.Controls.Add(this.panelCardDemo);
+            this.panelCard.Controls.Add(this.txtCardId);
             this.panelCard.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelCard.Location = new System.Drawing.Point(0, 0);
             this.panelCard.Name = "panelCard";
-            this.panelCard.Size = new System.Drawing.Size(800, 450);
+            this.panelCard.Size = new System.Drawing.Size(1379, 735);
             this.panelCard.TabIndex = 0;
+            // 
+            // txtCardId
+            // 
+            this.txtCardId.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtCardId.Location = new System.Drawing.Point(129, 212);
+            this.txtCardId.Name = "txtCardId";
+            this.txtCardId.Size = new System.Drawing.Size(316, 22);
+            this.txtCardId.TabIndex = 0;
+            // 
+            // panelCardDemo
+            // 
+            this.panelCardDemo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelCardDemo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelCardDemo.Controls.Add(this.lblCardName);
+            this.panelCardDemo.Controls.Add(this.lblCardType);
+            this.panelCardDemo.Controls.Add(this.pbCardDemo);
+            this.panelCardDemo.Location = new System.Drawing.Point(600, 159);
+            this.panelCardDemo.Name = "panelCardDemo";
+            this.panelCardDemo.Size = new System.Drawing.Size(733, 339);
+            this.panelCardDemo.TabIndex = 1;
+            // 
+            // pbCardDemo
+            // 
+            this.pbCardDemo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbCardDemo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCardDemo.Location = new System.Drawing.Point(476, 20);
+            this.pbCardDemo.Name = "pbCardDemo";
+            this.pbCardDemo.Size = new System.Drawing.Size(215, 236);
+            this.pbCardDemo.TabIndex = 0;
+            this.pbCardDemo.TabStop = false;
+            // 
+            // lblCardType
+            // 
+            this.lblCardType.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCardType.AutoSize = true;
+            this.lblCardType.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCardType.Location = new System.Drawing.Point(174, 52);
+            this.lblCardType.Name = "lblCardType";
+            this.lblCardType.Size = new System.Drawing.Size(194, 29);
+            this.lblCardType.TabIndex = 1;
+            this.lblCardType.Text = "CONTRACTOR";
+            // 
+            // lblCardName
+            // 
+            this.lblCardName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCardName.AutoSize = true;
+            this.lblCardName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCardName.Location = new System.Drawing.Point(56, 294);
+            this.lblCardName.Name = "lblCardName";
+            this.lblCardName.Size = new System.Drawing.Size(74, 25);
+            this.lblCardName.TabIndex = 2;
+            this.lblCardName.Text = "NAME";
+            this.lblCardName.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblCardIdno
+            // 
+            this.lblCardIdno.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCardIdno.AutoSize = true;
+            this.lblCardIdno.Location = new System.Drawing.Point(65, 216);
+            this.lblCardIdno.Name = "lblCardIdno";
+            this.lblCardIdno.Size = new System.Drawing.Size(43, 17);
+            this.lblCardIdno.TabIndex = 2;
+            this.lblCardIdno.Text = "ID No";
+            // 
+            // btnCardSearch
+            // 
+            this.btnCardSearch.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCardSearch.Location = new System.Drawing.Point(486, 210);
+            this.btnCardSearch.Name = "btnCardSearch";
+            this.btnCardSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnCardSearch.TabIndex = 3;
+            this.btnCardSearch.Text = "Search";
+            this.btnCardSearch.UseVisualStyleBackColor = true;
+            this.btnCardSearch.Click += new System.EventHandler(this.btnCardSearch_Click);
+            // 
+            // btnCardPrint
+            // 
+            this.btnCardPrint.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCardPrint.Location = new System.Drawing.Point(204, 363);
+            this.btnCardPrint.Name = "btnCardPrint";
+            this.btnCardPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnCardPrint.TabIndex = 4;
+            this.btnCardPrint.Text = "Print";
+            this.btnCardPrint.UseVisualStyleBackColor = true;
             // 
             // FormCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1379, 735);
             this.Controls.Add(this.panelCard);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormCard";
             this.Text = "FormCard";
+            this.panelCard.ResumeLayout(false);
+            this.panelCard.PerformLayout();
+            this.panelCardDemo.ResumeLayout(false);
+            this.panelCardDemo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCardDemo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +162,13 @@ namespace gui
         #endregion
 
         private System.Windows.Forms.Panel panelCard;
+        private System.Windows.Forms.Button btnCardPrint;
+        private System.Windows.Forms.Button btnCardSearch;
+        private System.Windows.Forms.Label lblCardIdno;
+        private System.Windows.Forms.Panel panelCardDemo;
+        private System.Windows.Forms.Label lblCardName;
+        private System.Windows.Forms.Label lblCardType;
+        private System.Windows.Forms.PictureBox pbCardDemo;
+        private System.Windows.Forms.TextBox txtCardId;
     }
 }
--- GuestRegistrationWinForm/FormCard.cs
+++ GuestRegistrationWinForm/FormCard.cs
@@ -2,19 +2,113 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using GenerateDocument.Library;
+using GuestDataManager.Library.DataAccess;
+using GuestRegistrationDesktopUI.Library;
+using GuestRegistrationDesktopUI.Library.Api;
+using GuestRegistrationDesktopUI.Library.CentralHub;
+using GuestRegistrationDesktopUI.Library.Models;
+using GuestRegistrationDesktopUI.Library.OCR;
+using GuestRegistrationWinForm;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TesseractOCR.Library;
 
 namespace gui
 {
     public partial class FormCard : Form
     {
+
+        public ICentralHub _centralHub;
+        private ScannedFileModel _scannedFileInfo;
+        private ScannedData _scannedData;
+        private CameraStatus _cameraStatus;
+        private ConsultantApplicationForm _consultantApplicationForm;
+        private VisitorDataSheet _visitorDataSheet;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public FormCard()
         {
             InitializeComponent();
+        }
+
+        private void btnCardSearch_Click(object sender, EventArgs e)
+        {
+            RetriveDBinfo retriveDBinfo = new RetriveDBinfo();
+            try
+            {
+                VisitorInformation visitor = retriveDBinfo.GetVisitorByIdNumber(txtCardId.Text);
+                ReloadDataToUi(visitor);
+                //_scannedData.isDataFromDb[0] = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"No Previous Visit Information!!");
+            }
+        }
+        public void ReloadDataToUi(VisitorInformation visitor)
+        {
+            if (!string.IsNullOrEmpty(visitor.IdNumber))
+            {
+                //scannedData
+                lblCardName.Text =  visitor.Name?.ToString();
+                //_scannedData.isDataFromDb[0] = true;
+                UpdatePhotoImageFromDb(ConvertBinaryToImage(Convert.FromBase64String(visitor.Photo)));
+
+                UpdateImageDetails(visitor);
+
+            }
+            else
+            {
+                MessageBox.Show("No information matched in Database!");
+            }
+
+        }
+
+        public void UpdateImageDetails(VisitorInformation visitor)
+        {
+            if (visitor.Photo.Length > 100)
+            {
+               // _scannedData.isDataFromDb[2] = true;
+                File.WriteAllBytes(gCONSTANTS.TEMPPHOTOFILEPATH, Convert.FromBase64String(visitor.Photo));
+            }
+        }
+        public Image ConvertBinaryToImage(byte[] binaryData)
+        {
+
+            using (MemoryStream memoryStream = new MemoryStream(binaryData))
+            {
+                // Create Image from binary data
+                Image image = Image.FromStream(memoryStream);
+                return image;
+            }
+        }
+        public void UpdatePhotoImageFromDb(Image image)
+        {
+            // Resize the image to fit the PictureBox
+            Bitmap resizedImage = new Bitmap(pbCardDemo.Width, pbCardDemo.Height);
+
+            using (Graphics g = Graphics.FromImage(resizedImage))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(image, 0, 0, pbCardDemo.Width, pbCardDemo.Height);
+            }
+
+            // Set the PictureBox properties
+            pbCardDemo.SizeMode = PictureBoxSizeMode.Zoom;
+            pbCardDemo.Image = resizedImage;
         }
     }
 }
